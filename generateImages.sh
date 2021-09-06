43686dd401501bd8359ad6bca40c2a4e387cdc63
DOS <dsaban@amadeus.com>
2021-09-06 13:42:57 +0200

    Adding main island. Change the orientation of cards to gain some space

--- generateImages.sh
+++ generateImages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-rm -rf _cards* _board _output imagesToPrint
+rm -rf _cards* _board _output
 ruby src/ruby/deck.rb
 ruby src/ruby/boss_1e_gene.rb
 cd ../printableCardsAppender
-./gradlew appendCard --args="../pokemon-cardgame/_output ../pokemon-cardgame/imagesToPrint/cards A4 true"
-./gradlew appendCard --args="../pokemon-cardgame/_boss ../pokemon-cardgame/imagesToPrint/boss A4 true"
+./gradlew appendCard --args="../pokemon-cardgame/_output ../pokemon-cardgame/imagesToPrint/cards A4 false"
+./gradlew appendCard --args="../pokemon-cardgame/_boss ../pokemon-cardgame/imagesToPrint/boss A4 false"
